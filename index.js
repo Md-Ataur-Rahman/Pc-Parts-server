7eca22a230ba87c74d95e651c24de487630ba71d
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-25 21:53:13 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -18,6 +19,21 @@ const client = new MongoClient(uri, {
 
 console.log(uri);
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -25,6 +41,7 @@ async function run() {
     const ordersCollection = client.db("pc_parts").collection("orders");
     const paymentCollection = client.db("pc_parts").collection("payments");
     const reviewCollection = client.db("pc_parts").collection("reviews");
+    const userCollection = client.db("pc_parts").collection("users");
 
     app.post("/create-payment-intent", async (req, res) => {
       const paymentService = req.body;
@@ -100,6 +117,22 @@ async function run() {
       const cursor = reviewCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
   } finally {
   }
