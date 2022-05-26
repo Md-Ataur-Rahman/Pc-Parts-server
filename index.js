fdb167bc1e738ae1a04afa5802e0dde33e68bba8
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-26 09:18:55 +0600

    add products setup done

--- index.js
+++ index.js
@@ -185,6 +185,13 @@ async function run() {
       const result = await userProfileCollection.findOne(query);
       res.send(result);
     });
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await toolsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
